2a8193b047d995cbdf41963f9c802e44702d2592
Daniel Labios <labiosd@gmail.com>
2020-09-22 23:51:44 -0400

    Alpha not Implemented, good enough for turn in

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,124 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    hue: '188',
+    saturation: '26',
+    lightness: '55',
+    alpha: '1',
+  }
+
+  handleHueChange = event => {
+    this.setState({ hue: event.target.value })
+  }
+  handleSaturationChange = event => {
+    this.setState({ saturation: event.target.value })
+  }
+  handleLightnessChange = event => {
+    this.setState({ lightness: event.target.value })
+  }
+  //  handleAlphaChange = event => {
+  //   this.setState({ alpha: event.target.value })
+  // }
+
   render() {
     return (
       <body2>
         <main>
           <section>
-            <h1>Color Picker HSL</h1>
+            <h1
+              style={{
+                color: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+              }}
+            >
+              Color Picker HSL
+            </h1>
           </section>
           <section>
-            <ul>
-              <li>HN</li>
-              <li>SN</li>
-              <li>LN</li>
-              <li>AN</li>
+            <ul
+              style={{
+                color: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+              }}
+            >
+              <li>{this.state.hue}</li>
+              <li>{this.state.saturation}</li>
+              <li>{this.state.lightness}</li>
+              <li>{this.state.alpha}</li>
             </ul>
             <ul>
               <li>
-                <a>Hue</a>
-                <input type="range" min="1" max="360"></input>
+                <a
+                  style={{
+                    color: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+                  }}
+                >
+                  Hue
+                </a>
+                <input
+                  type="range"
+                  min="1"
+                  max="360"
+                  value={this.state.hue}
+                  onChange={this.handleHueChange}
+                ></input>
               </li>
               <li>
-                <a>Saturation</a>
-                <input type="range" min="0" max="100"></input>
+                <a
+                  style={{
+                    color: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+                  }}
+                >
+                  Saturation
+                </a>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={this.state.saturation}
+                  onChange={this.handleSaturationChange}
+                ></input>
               </li>
               <li>
-                <a>Lightness</a>
-                <input type="range" min="0" max="100"></input>
+                <a
+                  style={{
+                    color: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+                  }}
+                >
+                  Lightness
+                </a>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={this.state.lightness}
+                  onChange={this.handleLightnessChange}
+                ></input>
               </li>
               <li>
-                <a>Alpha</a>
-                <input type="range" min="0" max="100"></input>
+                <a
+                  style={{
+                    color: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+                  }}
+                >
+                  Alpha
+                </a>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  // value={this.state.hue}
+                  // onChange={this.handleHueChange}
+                ></input>
               </li>
             </ul>
           </section>
           <section>
             <div2>
-              <div3></div3>
+              <div3
+                style={{
+                  background: `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`,
+                }}
+              ></div3>
             </div2>
           </section>
         </main>
